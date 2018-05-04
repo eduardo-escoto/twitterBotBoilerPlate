d3cbb325e8163d57ec3202e2a961032da1702769
Eduardo Escoto <eduardo@escoto.me>
2018-05-03 23:29:31 -0700

    Added gitignore and created general tweet functions

--- bot.js
+++ bot.js
@@ -6,15 +6,16 @@ let twit = require('twit');
 let apiData = require('./apiKeys.js');
 let Twitter = new twit(apiData);
 let lastColor;
+const tweetIntervalInMilliseconds = 1000 * 60 * 60 * 6; //setinterval uses milliseconds, this for examples is 6 hours
 
 function postTweet(tweet) {
     let path = 'statuses/update';
     Twitter.post(path, tweet, (err, data, response) => console.log("Tweet Posted Successfuly"));
 }
 
-function tweetImage(color) {
+function tweetImage(path) {
     let fs = require('fs');
-    const image_path = './image.png';
+    const image_path = path;//local path to image to tweet
     let b64content = fs.readFileSync(image_path, {
         encoding: 'base64'
     });
@@ -22,67 +23,26 @@ function tweetImage(color) {
         media_data: b64content
     }, (err, data, response) => {
         media_ids = new Array(data.media_id_string);
-        const tweetData = generateTweetData(color, media_ids);
-        console.log("Tweeting the color: " + color.hex.combined);
+        const tweetData = generateTweetData(media_ids);
+        console.log("Tweeting the image: " + path);
         postTweet(tweetData);
     });
 
 }
-
-function generateTweetData(color, media_ids) {
-    const status =
-        `💖💖💖💖💖💖💖💖💖💖💖💖💖💖
-🌸 NAME: ${color.nameData.closestMatchName} 🌸
-🌸 HEX: ${color.hex.combined} 🌸
-🌸 RGB: (${color.rgb.red}, ${color.rgb.green}, ${color.rgb.blue}) 🌸
-🌸 HSL: (${color.hsl.hueData.string}, ${color.hsl.saturationData.string}, ${color.hsl.lightnessData.string}) 🌸
-💖💖💖💖💖💖💖💖💖💖💖💖💖💖`;
+function generateImageTweetData(status, media_ids) {
     return {
         media_ids,
         status
     }
 }
-
-function saveColor(color) {
-    let fs = require('fs');
-    let Jimp = require('jimp');
-    let image = new Jimp(1080, 1920, color.hex.raw);
-    image.quality(100);
-    image.write('image.png', () => {
-        console.log(`Saved image for color ${color.hex.combined}`);
-        tweetImage(color)
-    });
-}
-
-function generatePastelTweet() {
-    const pastelGenerator = require('./pastelGenerator.js');
-    const color = pastelGenerator.generateTweetColor();
-    if (compareColorByHue(lastColor, color)) {
-        saveColor(color);
-        lastColor = color;
-    } else {
-        console.log(`Colors too similar colors were: ${lastColor.hex.combined} and ${color.hex.combined}`);
-        generatePastelTweet();
+function generateTextTweetData(status) {
+    return {
+        status
     }
 }
 
-function compareColorByHue(lastColor, nextColor) {
-    if (lastColor) {
-        const lastHuePercentage = Number((lastColor.hsl.hueData.value / 360).toFixed(5));
-        const nextHuePercentage = Number((nextColor.hsl.hueData.value / 360).toFixed(5));
-        const difference = Math.abs(lastHuePercentage - nextHuePercentage) * 100;
-        if (difference < 15) {
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        return true;
-    }
-}
-generatePastelTweet();
 console.log(`Starting the cycle.`)
 setInterval(() => {
     console.log("Running next cycle...");
-    generatePastelTweet();
-}, 1000*60*60*6);
+
+}, tweetIntervalInMilliseconds);
